036e730d4be9abf5e490daf31d0d98432feef32d
Michael Baker <michael.baker@bulletproof.net>
2016-12-24 07:34:06 +0800

    Update login.go

--- cmd/saml2aws/commands/login.go
+++ cmd/saml2aws/commands/login.go
@@ -8,7 +8,6 @@ import (
         "net/http"
         "io/ioutil"
         "strings"
-        "passwordhelper"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
