a3ebd9fac22f8a8bb6a3853e91950b9fcfab6228
David OBrien <me@david-obrien.net>
2017-01-09 16:13:41 +1100

    Add choco nuspec file for saml2aws.
    Add appveyor.yml file for Windows based build.
    
    Fix appveyor.yml
    
    Fix appveyor.yml
    
    Fix appveyor.yml
    
    Use downloaded appveyor.yml
    
    Jump back to clone folder, add artifacts
    
    Add owner and author to nuspec
    
    Add choco API key and push to choco
    
    Add push to choco
    
    Change GH release to draft for now
    
    Add Hash files as artifacts
    
    Fix environment variable ref
    
    Add Go test run
    
    Add install of InvokeBuild
    
    Add install of nuget provider
    
    Add Invoke-Build build script and refactor code in there.
    
    Set Erroractionpreference to Continue before glide install
    
    Fix quoting in build script
    
    FIx parameters in appveyor.yml
    
    Fixing env Var in appveyor.yml
    
    Create SHA file during build.
    
    Update nuspec file with author and owner.

---
+++ default.build.ps1
@@ -0,0 +1,31 @@
+Set-StrictMode -Version 'Latest'
+
+#$nameOfApp = 'saml2aws'
+#$version = '1.3.2'
+
+task 'Compile Go libraries...' {
+  $ErrorActionPreference = 'Continue'
+  c:\gopath\bin\glide install 2> $null
+  $ErrorActionPreference = 'Stop'
+  go build -o "bin/${env:appName}.exe" -ldflags "-X main.Version=${env:appVersion}" "./cmd/$env:appName"
+}
+
+task 'Prepare for choco stuff...' {
+  mkdir ./choco/src
+  copy-item "./bin/${env:appName}.exe" "./choco/src/${env:appName}.exe"
+}
+
+task 'Pack Choco...' {
+  Set-Location choco
+  choco pack "${env:appName}.nuspec"
+  $hash = Get-FileHash "${env:appName}.${env:appVersion}.nupkg"
+  "$($hash.Hash) $(Split-Path $hash.Path -Leaf)" > "${env:appName}.${env:appVersion}.nupkg.sha256"
+}
+
+task 'Zip for GH release...' {
+  7z a "${env:appName}.zip" "$env:APPVEYOR_BUILD_FOLDER\bin\${env:appName}.exe"
+  $hash = Get-FileHash "${env:appName}.zip"
+  "$($hash.Hash) $(Split-Path $hash.Path -Leaf)" > "${env:appName}.zip.sha256"
+}
+
+task . 'Compile Go libraries...', 'Prepare for choco stuff...', 'Pack Choco...', 'Zip for GH release...'
