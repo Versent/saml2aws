3e948d6b3b96361f64c5f57e1362a7a589a9942f
Mark Wolfe <mark@wolfe.id.au>
2017-08-03 20:42:23 +1000

    fix(RoleSelection) Now returns the role if list contains a single entry

--- cmd/saml2aws/commands/login_test.go
+++ cmd/saml2aws/commands/login_test.go
@@ -16,3 +16,20 @@ func TestResolveLoginDetails(t *testing.T) {
 	assert.Empty(t, err)
 	assert.Equal(t, loginDetails, &saml2aws.LoginDetails{Username: "wolfeidau", Password: "testtestlol", Hostname: "id.example.com"})
 }
+
+func TestResolveRoleSingleEntry(t *testing.T) {
+
+	adminRole := &saml2aws.AWSRole{
+		Name:         "admin",
+		RoleARN:      "arn:aws:iam::456456456456:saml-provider/example-idp,arn:aws:iam::456456456456:role/admin",
+		PrincipalARN: "arn:aws:iam::456456456456:role/admin,arn:aws:iam::456456456456:saml-provider/example-idp",
+	}
+
+	awsRoles := []*saml2aws.AWSRole{
+		adminRole,
+	}
+
+	got, err := resolveRole(awsRoles, "", &LoginFlags{})
+	assert.Empty(t, err)
+	assert.Equal(t, got, adminRole)
+}
