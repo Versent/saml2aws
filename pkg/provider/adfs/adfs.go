6363cf7210ac5bbd994da4fd2d12e1e64ab2482d
Anthony Engelstad <anthony.engelstad@versent.com.au>
2017-10-20 21:54:03 +1100

    feat(ADFS3) Add Symantec VIP MFA support to saml2aws

--- pkg/provider/adfs/adfs.go
+++ pkg/provider/adfs/adfs.go
@@ -1,10 +1,8 @@
 package adfs
 
 import (
-	"bytes"
 	"crypto/tls"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -12,14 +10,21 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/versent/saml2aws/pkg/cfg"
 	"github.com/versent/saml2aws/pkg/creds"
+	"github.com/versent/saml2aws/pkg/dump"
+	"github.com/versent/saml2aws/pkg/prompter"
 	"github.com/versent/saml2aws/pkg/provider"
 )
 
+var logger = logrus.WithField("provider", "adfs")
+
 // Client wrapper around ADFS enabling authentication and retrieval of assertions
 type Client struct {
-	client *provider.HTTPClient
+	client     *provider.HTTPClient
+	idpAccount *cfg.IDPAccount
+	prompter   prompter.Prompter
 }
 
 // New create a new ADFS client
@@ -35,7 +40,9 @@ func New(idpAccount *cfg.IDPAccount) (*Client, error) {
 	}
 
 	return &Client{
-		client: client,
+		client:     client,
+		idpAccount: idpAccount,
+		prompter:   prompter.NewCli(),
 	}, nil
 }
 
@@ -43,7 +50,6 @@ func New(idpAccount *cfg.IDPAccount) (*Client, error) {
 func (ac *Client) Authenticate(loginDetails *creds.LoginDetails) (string, error) {
 	var authSubmitURL string
 	var samlAssertion string
-	authForm := url.Values{}
 
 	adfsURL := fmt.Sprintf("https://%s/adfs/ls/IdpInitiatedSignOn.aspx?loginToRp=urn:amazon:webservices", loginDetails.Hostname)
 
@@ -56,6 +62,8 @@ func (ac *Client) Authenticate(loginDetails *creds.LoginDetails) (string, error)
 	if err != nil {
 		return samlAssertion, errors.Wrap(err, "failed to build document from response")
 	}
+
+	authForm := url.Values{}
 
 	doc.Find("input").Each(func(i int, s *goquery.Selection) {
 		updateFormData(authForm, s, loginDetails)
@@ -82,21 +90,28 @@ func (ac *Client) Authenticate(loginDetails *creds.LoginDetails) (string, error)
 
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
+	logger.WithField("authSubmitURL", authSubmitURL).WithField("req", dump.RequestString(req)).Debug("POST")
+
 	res, err = ac.client.Do(req)
 	if err != nil {
-		return samlAssertion, errors.Wrap(err, "error retieving login form")
+		return samlAssertion, errors.Wrap(err, "error retrieving login form results")
 	}
 
 	//log.Printf("res code = %v status = %s", res.StatusCode, res.Status)
+	logger.WithField("authSubmitURL", authSubmitURL).WithField("res", dump.ResponseString(res)).Debug("POST")
 
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return samlAssertion, errors.Wrap(err, "error retieving body")
+	switch ac.idpAccount.MFA {
+	case "VIP":
+		res, err = ac.vipMFA(authSubmitURL, res)
+		if err != nil {
+			return samlAssertion, errors.Wrap(err, "error retrieving mfa form results")
+		}
 	}
 
-	doc, err = goquery.NewDocumentFromReader(bytes.NewBuffer(data))
+	// just parse the response whether res is from the login form or MFA form
+	doc, err = goquery.NewDocumentFromResponse(res)
 	if err != nil {
-		return samlAssertion, errors.Wrap(err, "error parsing document")
+		return samlAssertion, errors.Wrap(err, "error retrieving login response body")
 	}
 
 	doc.Find("input").Each(func(i int, s *goquery.Selection) {
@@ -114,6 +129,56 @@ func (ac *Client) Authenticate(loginDetails *creds.LoginDetails) (string, error)
 	})
 
 	return samlAssertion, nil
+}
+
+// vipMFA when supplied with the the form response document attempt to extract the VIP mfa related field
+// then use that to trigger a submit of the MFA security token
+func (ac *Client) vipMFA(authSubmitURL string, res *http.Response) (*http.Response, error) {
+
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		return nil, errors.Wrap(err, "error retrieving saml response body")
+	}
+
+	otpForm := url.Values{}
+
+	vipIndex := doc.Find("input#authMethod[value=VIPAuthenticationProviderWindowsAccountName]").Index()
+
+	if vipIndex == -1 {
+		return nil, errors.New("unable to find VIP challenge, no MFA found")
+	}
+
+	var token = ac.prompter.RequestSecurityCode("000000")
+
+	doc.Find("input").Each(func(i int, s *goquery.Selection) {
+		updateOTPFormData(otpForm, s, token)
+	})
+
+	doc.Find("form").Each(func(i int, s *goquery.Selection) {
+		action, ok := s.Attr("action")
+		if !ok {
+			return
+		}
+		authSubmitURL = action
+	})
+
+	if authSubmitURL == "" {
+		return nil, fmt.Errorf("unable to locate IDP MFA form submit URL")
+	}
+
+	req, err := http.NewRequest("POST", authSubmitURL, strings.NewReader(otpForm.Encode()))
+	if err != nil {
+		return nil, errors.Wrap(err, "error building MFA request")
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err = ac.client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "error retrieving content")
+	}
+
+	return res, nil
 }
 
 func updateFormData(authForm url.Values, s *goquery.Selection, user *creds.LoginDetails) {
@@ -137,4 +202,24 @@ func updateFormData(authForm url.Values, s *goquery.Selection, user *creds.Login
 		}
 		authForm.Add(name, val)
 	}
+}
+
+func updateOTPFormData(otpForm url.Values, s *goquery.Selection, token string) {
+	name, ok := s.Attr("name")
+	//	log.Printf("name = %s ok = %v", name, ok)
+	if !ok {
+		return
+	}
+	lname := strings.ToLower(name)
+	if strings.Contains(lname, "security_code") {
+		otpForm.Add(name, token)
+	} else {
+		// pass through any hidden fields
+		val, ok := s.Attr("value")
+		if !ok {
+			return
+		}
+		otpForm.Add(name, val)
+	}
+
 }
