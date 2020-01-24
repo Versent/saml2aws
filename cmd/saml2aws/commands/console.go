5eeed3a7be5939225e0a7a54f24a1a6c48de550c
Mark Wolfe <mark.wolfe@versent.com.au>
2020-01-24 10:01:25 +1000

    feat(console) Log into the AWS console from saml2aws
    As per https://segment.com/blog/secure-access-to-100-aws-accounts/ I have implemented console log in directly from saml2aws.
    
    Note: Like the exec command, if your credentials are still active this will not prompt for login.

---
+++ cmd/saml2aws/commands/console.go
@@ -0,0 +1,128 @@
+package commands
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/skratchdot/open-golang/open"
+	"github.com/versent/saml2aws/pkg/awsconfig"
+	"github.com/versent/saml2aws/pkg/flags"
+)
+
+// Exec execute the supplied command after seeding the environment
+func Console(consoleFlags *flags.LoginExecFlags) error {
+
+	account, err := buildIdpAccount(consoleFlags)
+	if err != nil {
+		return errors.Wrap(err, "error building login details")
+	}
+
+	sharedCreds := awsconfig.NewSharedCredentials(account.Profile)
+
+	// this checks if the credentials file has been created yet
+	// can only really be triggered if saml2aws exec is run on a new
+	// system prior to creating $HOME/.aws
+	exist, err := sharedCreds.CredsExists()
+	if err != nil {
+		return errors.Wrap(err, "error loading credentials")
+	}
+	if !exist {
+		fmt.Println("unable to load credentials, login required to create them")
+		return nil
+	}
+
+	awsCreds, err := sharedCreds.Load()
+	if err != nil {
+		return errors.Wrap(err, "error loading credentials")
+	}
+
+	if awsCreds.Expires.Sub(time.Now()) < 0 {
+		return errors.New("error aws credentials have expired")
+	}
+
+	ok, err := checkToken(account.Profile)
+	if err != nil {
+		return errors.Wrap(err, "error validating token")
+	}
+
+	if !ok {
+		err = Login(consoleFlags)
+	}
+	if err != nil {
+		return errors.Wrap(err, "error logging in")
+	}
+
+	if consoleFlags.ExecProfile != "" {
+		// Assume the desired role before generating env vars
+		awsCreds, err = assumeRoleWithProfile(consoleFlags.ExecProfile, consoleFlags.CommonFlags.SessionDuration)
+		if err != nil {
+			return errors.Wrap(err,
+				fmt.Sprintf("error acquiring credentials for profile: %s", consoleFlags.ExecProfile))
+		}
+	}
+
+	fmt.Printf("Opening console for profile %s ...\n", account.Profile)
+
+	return federatedLogin(awsCreds, consoleFlags)
+}
+
+func federatedLogin(creds *awsconfig.AWSCredentials, consoleFlags *flags.LoginExecFlags) error {
+	jsonBytes, err := json.Marshal(map[string]string{
+		"sessionId":    creds.AWSAccessKey,
+		"sessionKey":   creds.AWSSecretKey,
+		"sessionToken": creds.AWSSessionToken,
+	})
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("GET", "https://signin.aws.amazon.com/federation", nil)
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	q.Add("Action", "getSigninToken")
+	q.Add("Session", string(jsonBytes))
+
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Call to getSigninToken failed with %v", resp.Status)
+	}
+
+	var respParsed map[string]string
+	if err = json.Unmarshal([]byte(body), &respParsed); err != nil {
+		return err
+	}
+
+	signinToken, ok := respParsed["SigninToken"]
+	if !ok {
+		return err
+	}
+
+	destination := "https://console.aws.amazon.com/"
+
+	loginURL := fmt.Sprintf(
+		"https://signin.aws.amazon.com/federation?Action=login&Issuer=aws-okta&Destination=%s&SigninToken=%s",
+		url.QueryEscape(destination),
+		url.QueryEscape(signinToken),
+	)
+
+	return open.Run(loginURL)
+}
