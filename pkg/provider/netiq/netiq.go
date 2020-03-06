03dbe8cb56f3dacdcf6469ed539b5555f3402bcf
Ilya Trofimov <ilya.v.trofimov@gmail.com>
2020-03-06 12:56:36 +1100

    Implemented Follow redirects approach

--- pkg/provider/netiq/netiq.go
+++ pkg/provider/netiq/netiq.go
@@ -1,6 +1,7 @@
 package netiq
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -9,9 +10,14 @@ import (
 	"github.com/versent/saml2aws/pkg/page"
 	"github.com/versent/saml2aws/pkg/prompter"
 	"github.com/versent/saml2aws/pkg/provider"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"regexp"
+	"strings"
 )
+
+var logger = logrus.WithField("provider", "NetIQ")
 
 type Client struct {
 	client *provider.HTTPClient
@@ -28,7 +34,139 @@ func New(idpAccount *cfg.IDPAccount) (*Client, error) {
 
 }
 
+const isFollow = true
+const samlURL = "/nidp/saml2/idpsend?PID=STSPv8a5kc"
+
 func (nc *Client) Authenticate(loginDetails *creds.LoginDetails) (string, error) {
+	if isFollow {
+		req, err := http.NewRequest("GET", loginDetails.URL+samlURL, nil)
+		if err != nil {
+			return "", errors.Wrap(err, "Error building request")
+		}
+		return nc.follow(req, loginDetails)
+	} else {
+		return nc.staticFlow(loginDetails)
+	}
+}
+
+func (nc *Client) follow(req *http.Request, loginDetails *creds.LoginDetails) (string, error) {
+	debugRequest(req)
+	resp, err := nc.client.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to perform http request to "+req.URL.String())
+	}
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to build document from response")
+	}
+	debugResponse(resp)
+	if isSAMLResponse(doc) {
+		return extractSAMLAssertion(doc)
+	} else if resourcePath, isGetToContext := extractGetToContentUrl(doc); isGetToContext {
+		newReq, err := buildGetToContentRequest(loginDetails.URL + resourcePath + "&uiDestination=contentDiv")
+		if err != nil {
+			return "", errors.Wrap(err, "Error building request")
+		}
+		return nc.follow(newReq, loginDetails)
+	} else if resourceURL, isWinLocHref := extractWinLocHrefURL(doc); isWinLocHref {
+		newReq, err := buildGetToContentRequest(resourceURL)
+		if err != nil {
+			return "", errors.Wrap(err, "Error building request")
+		}
+		return nc.follow(newReq, loginDetails)
+	} else if form, isIDPLoginPass := extractIDPLoginPass(doc); isIDPLoginPass {
+		form.Values.Set("Ecom_User_ID", loginDetails.Username)
+		form.Values.Set("Ecom_Password", loginDetails.Password)
+		newReq, err := form.BuildRequest()
+		if err != nil {
+			return "", errors.Wrap(err, "Error building request")
+		}
+		return nc.follow(newReq, loginDetails)
+	} else if form, isIDPLoginRsa := extractIDPLoginRsa(doc); isIDPLoginRsa {
+		token := prompter.StringRequired("Enter concatenated pin and token")
+		form.Values.Set("Ecom_User_ID", loginDetails.Username)
+		form.Values.Set("Ecom_Token", token)
+		newReq, err := form.BuildRequest()
+		if err != nil {
+			return "", errors.Wrap(err, "Error building request")
+		}
+		return nc.follow(newReq, loginDetails)
+	} else {
+		return "", fmt.Errorf("unknown document type")
+	}
+}
+
+func isSAMLResponse(doc *goquery.Document) bool {
+	return doc.Find("input[name=\"SAMLResponse\"]").Size() == 1
+}
+
+func extractGetToContentUrl(doc *goquery.Document) (string, bool) {
+	script := doc.Find("body script:contains('getToContent')")
+	if script.Size() != 1 {
+		return "", false
+	}
+	re := regexp.MustCompile(`getToContent\('(.*)',.*\);`)
+	match := re.FindStringSubmatch(strings.TrimSpace(script.Text()))
+	if len(match) == 2 {
+		return match[1], true
+	} else {
+		return "", false
+	}
+}
+
+func extractWinLocHrefURL(doc *goquery.Document) (string, bool) {
+	script := doc.Find("body script:contains('window.location.href')")
+	if script.Size() != 1 {
+		return "", false
+	}
+	re := regexp.MustCompile(`window.location.href='(.*)';`)
+	match := re.FindStringSubmatch(strings.TrimSpace(script.Text()))
+	if len(match) == 2 {
+		return match[1], true
+	} else {
+		return "", false
+	}
+}
+
+func extractIDPLoginPass(doc *goquery.Document) (*page.Form, bool) {
+	idpPoginForm := doc.Find("body form:has(input[name=\"Ecom_Password\"])")
+	if idpPoginForm.Size() != 1 {
+		return nil, false
+	}
+	action, exists := idpPoginForm.Attr("action")
+	if !exists {
+		return nil, false
+	}
+	form := &page.Form{
+		URL:    action,
+		Method: "POST",
+		Values: &url.Values{},
+	}
+	return form, true
+}
+
+func extractIDPLoginRsa(doc *goquery.Document) (*page.Form, bool) {
+	idpPoginForm := doc.Find("body form:has(input[name=\"Ecom_Token\"])")
+	if idpPoginForm.Size() != 1 {
+		return nil, false
+	}
+	action, exists := idpPoginForm.Attr("action")
+	if !exists {
+		return nil, false
+	}
+	form := &page.Form{
+		URL:    action,
+		Method: "POST",
+		Values: &url.Values{},
+	}
+	return form, true
+}
+
+func buildGetToContentRequest(resourceURL string) (*http.Request, error) {
+	return http.NewRequest("GET", resourceURL, nil)
+}
+
+func (nc *Client) staticFlow(loginDetails *creds.LoginDetails) (string, error) {
 	_, err := nc.getAppPortalLoginForm(loginDetails)
 	if err != nil {
 		return "", errors.Wrap(err, "error logging into Applications portal")
@@ -49,9 +187,13 @@ func (nc *Client) Authenticate(loginDetails *creds.LoginDetails) (string, error)
 	if err != nil {
 		return "", errors.Wrap(err, "error getting saml response")
 	}
-	debugResponse(samlRes)
 
-	samlAssertion, err := nc.extractSAMLAssertion(samlRes)
+	doc, err := goquery.NewDocumentFromReader(samlRes.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "Can't extract SAML assertion from")
+	}
+	debugResponse(samlRes)
+	samlAssertion, err := extractSAMLAssertion(doc)
 	if err != nil {
 		return "", errors.Wrap(err, "error extracting saml assertion from saml response")
 	}
@@ -63,12 +205,36 @@ func (nc *Client) getAppPortalLoginForm(loginDetails *creds.LoginDetails) (*http
 }
 
 func debugResponse(resp *http.Response) {
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err == nil {
-		logrus.
-			WithField("body", doc.Text()).
-			WithFields(logrus.Fields{"Status": resp.Status}).Debug("HTTP Resp Body (DEBUG)")
+	var respBodyStr = ""
+	if resp.Body != nil {
+		respBody, _ := ioutil.ReadAll(resp.Body)
+		respBodyStr = string(respBody)
 	}
+	logger.
+		WithField("RequestMethod", resp.Request.Method).
+		WithField("RequestURL", resp.Request.URL).
+		WithField("ResponseBody", respBodyStr).
+		WithField("ResponseStatus", resp.Status).
+		Debug("HTTP Response (DEBUG)")
+}
+
+func debugRequest(req *http.Request) {
+	var reqBodyStr = ""
+	if req.Body != nil {
+		reqBody, _ := ioutil.ReadAll(req.Body)
+		reqBodyStr = string(reqBody)
+	}
+	logger.
+		WithField("Method", req.Method).
+		WithField("URL", req.URL).
+		WithField("Body", reqBodyStr).
+		Debug("HTTP Request (DEBUG)")
+}
+
+func logDocDetected(docType string) {
+	logger.
+		WithField("type", docType).
+		Debug("doc detect")
 }
 
 func (nc *Client) loginToAppPortal(loginDetails *creds.LoginDetails) (*http.Response, error) {
@@ -107,17 +273,13 @@ func (nc *Client) rsaAuth(loginDetails *creds.LoginDetails) (*http.Response, err
 }
 
 func (nc *Client) getSAMLResponse(loginDetails *creds.LoginDetails) (*http.Response, error) {
-	return nc.client.Get(loginDetails.URL + "/nidp/saml2/idpsend?PID=STSPv8a5kc")
+	return nc.client.Get(loginDetails.URL + samlURL)
 }
 
-func (nc *Client) extractSAMLAssertion(resp *http.Response) (string, error) {
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		return "", errors.Wrap(err, "Can't extract SAML assertion from")
-	}
+func extractSAMLAssertion(doc *goquery.Document) (string, error) {
 	samlAssertion, ok := doc.Find("input[name=\"SAMLResponse\"]").Attr("value")
 	if !ok {
-		return "", errors.Wrap(err, "No SAML assertion found in response")
+		return "", fmt.Errorf("no SAML assertion in response")
 	}
 	return samlAssertion, nil
 }
