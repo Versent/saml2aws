d828f086d4c85173947f3bce848b614e68239c29
Michael Palmer <MichaelPalmer1@users.noreply.github.com>
2020-09-24 13:15:19 -0400

    Update default.build.ps1

--- default.build.ps1
+++ default.build.ps1
@@ -1,31 +1,30 @@
 Set-StrictMode -Version 'Latest'
 
-#$nameOfApp = 'gossamer3'
-$version = ${env:APPVEYOR_REPO_TAG_NAME}.Split('v')[1]
+$version = ${env:VERSION}
 
 task 'Compile Go libraries...' {
   $ErrorActionPreference = 'Continue'
-  go build -o "bin/${env:appName}.exe" -ldflags "-X main.Version=${version}" "./cmd/$env:appName"
+  go build -o "bin/gossamer3.exe" -ldflags "-X main.Version=${version}" "./cmd/gossamer3"
 }
 
 task 'Prepare for choco stuff...' {
   mkdir ./choco/src
-  copy-item "./bin/${env:appName}.exe" "./choco/src/${env:appName}.exe"
+  copy-item "./bin/gossamer3.exe" "./choco/src/gossamer3.exe"
   Copy-Item "./LICENSE.md" "./choco/src/LICENSE.md"
   Copy-Item "./choco/VERIFICATION.txt" "./choco/src/VERIFICATION.txt"
 }
 
 task 'Pack Choco...' {
   Set-Location choco
-  choco pack --version "$version" "${env:appName}.nuspec"
-  $hash = Get-FileHash "${env:appName}.${version}.nupkg"
-  "$($hash.Hash) $(Split-Path $hash.Path -Leaf)" > "${env:appName}.${version}.nupkg.sha256"
+  choco pack --version "$version" "gossamer3.nuspec"
+  $hash = Get-FileHash "gossamer3.${version}.nupkg"
+  "$($hash.Hash) $(Split-Path $hash.Path -Leaf)" > "gossamer3.${version}.nupkg.sha256"
 }
 
 task 'Zip for GH release...' {
-  7z a "${env:appName}.zip" "$env:APPVEYOR_BUILD_FOLDER\bin\${env:appName}.exe"
-  $hash = Get-FileHash "${env:appName}.zip"
-  "$($hash.Hash) $(Split-Path $hash.Path -Leaf)" > "${env:appName}.zip.sha256"
+  7z a "gossamer3.zip" "bin\gossamer3.exe"
+  $hash = Get-FileHash "gossamer3.zip"
+  "$($hash.Hash) $(Split-Path $hash.Path -Leaf)" > "gossamer3.zip.sha256"
 }
 
 task . 'Compile Go libraries...', 'Prepare for choco stuff...', 'Pack Choco...', 'Zip for GH release...'
