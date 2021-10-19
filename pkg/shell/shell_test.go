7b6c68ea2d7a551e88e3d6b4e844987946d2bca2
Randy Stauner <randy@r4s6.net>
2021-10-19 07:49:10 -0700

    Execute command directly without sh -c
    When the args are concatenated there is a loss of fidelity:
    arguments with spaces will be split and any special characters
    will be evaluated by the shell.
    If we simply execute the provided command it will propagate correctly
    and it can always be wrapped in a shell on the command line
    if that behavior is desired.
    
    fixes #236

--- pkg/shell/shell_test.go
+++ pkg/shell/shell_test.go
@@ -3,6 +3,7 @@
 package shell
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,5 +14,29 @@ func TestExecShellCmd(t *testing.T) {
 	err := ExecShellCmd([]string{"echo", "$TESTTEST"}, []string{"TESTTEST=123"})
 
 	assert.Nil(t, err)
+
+}
+
+func TestPrepCmd(t *testing.T) {
+
+	cmd := prepCmd([]string{"echo", "some$TESTTEST", "one   two"}, []string{"TESTTEST=123"})
+
+	var out strings.Builder
+	cmd.Stdout = &out
+	err := cmd.Run()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "some$TESTTEST one   two\n", out.String(), "no eval, spaces preserved")
+}
+
+func TestPrepCmdShell(t *testing.T) {
+	cmd := prepCmd([]string{"sh", "-c", "echo some$TESTTEST one   two"}, []string{"TESTTEST=123"})
+
+	var out strings.Builder
+	cmd.Stdout = &out
+	err := cmd.Run()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "some123 one two\n", out.String(), "var evaled, spaces squashed")
 
 }
