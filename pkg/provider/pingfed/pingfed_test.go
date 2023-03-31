d15785dbc7a93747312a93bd99e6d231185f4f98
Leo Liu <karta1041337@gmail.com>
2023-03-31 19:15:44 +0800

    Add unit test

--- pkg/provider/pingfed/pingfed_test.go
+++ pkg/provider/pingfed/pingfed_test.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
 	"testing"
 
@@ -12,6 +15,7 @@ import (
 	"github.com/versent/saml2aws/v2/mocks"
 	"github.com/versent/saml2aws/v2/pkg/creds"
 	"github.com/versent/saml2aws/v2/pkg/prompter"
+	"github.com/versent/saml2aws/v2/pkg/provider"
 )
 
 func TestMakeAbsoluteURL(t *testing.T) {
@@ -81,7 +85,7 @@ func TestHandleLogin(t *testing.T) {
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(data))
 	require.Nil(t, err)
 
-	_, req, err := ac.handleLogin(ctx, doc)
+	_, req, err := ac.handleLogin(ctx, doc, &url.URL{})
 	require.Nil(t, err)
 
 	b, err := io.ReadAll(req.Body)
@@ -103,8 +107,16 @@ func TestHandleOTP(t *testing.T) {
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(data))
 	require.Nil(t, err)
 
-	ac := Client{}
-	_, req, err := ac.handleOTP(context.Background(), doc)
+	jar, err := cookiejar.New(&cookiejar.Options{})
+	require.Nil(t, err)
+
+	opts := &provider.HTTPClientOptions{IsWithRetries: false}
+	ac := Client{client: &provider.HTTPClient{Client: http.Client{Jar: jar}, Options: opts}}
+	_, req, err := ac.handleOTP(context.Background(), doc, &url.URL{
+		Scheme: "https",
+		Host:   "authenticator.pingone.com",
+		Path:   "/pingid/ppm/auth/otp",
+	})
 	require.Nil(t, err)
 
 	b, err := io.ReadAll(req.Body)
@@ -122,7 +134,7 @@ func TestHandleFormRedirect(t *testing.T) {
 	require.Nil(t, err)
 
 	ac := Client{}
-	_, req, err := ac.handleFormRedirect(context.Background(), doc)
+	_, req, err := ac.handleFormRedirect(context.Background(), doc, &url.URL{})
 	require.Nil(t, err)
 
 	b, err := io.ReadAll(req.Body)
@@ -141,7 +153,7 @@ func TestHandleWebAuthn(t *testing.T) {
 	require.Nil(t, err)
 
 	ac := Client{}
-	_, req, err := ac.handleWebAuthn(context.Background(), doc)
+	_, req, err := ac.handleWebAuthn(context.Background(), doc, &url.URL{})
 	require.Nil(t, err)
 
 	b, err := io.ReadAll(req.Body)
