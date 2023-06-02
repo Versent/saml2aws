dbd8a170c55c33bf1c5d8340ccb860bcf86e3465
Keith McDuffee <kmcduffee@stackref.com>
2023-06-02 10:50:17 -0400

    Tabs not spaces

--- pkg/shell/shell.go
+++ pkg/shell/shell.go
@@ -15,13 +15,13 @@ func ExecShellCmd(cmdline []string, envVars []string) error {
 }
 
 func prepCmd(cmdline []string, envVars []string) *exec.Cmd {  
-  c := strings.Join(cmdline, " ")
+	c := strings.Join(cmdline, " ")
 
-  cs := []string{"/bin/sh", "-c", c}
-  cmd := exec.Command(cs[0], cs[1:]...)  
-  cmd.Stdin = os.Stdin  
-  cmd.Stdout = os.Stdout  
-  cmd.Stderr = os.Stderr  
-  cmd.Env = append(os.Environ(), envVars...)  
-  return cmd  
+	cs := []string{"/bin/sh", "-c", c}
+	cmd := exec.Command(cs[0], cs[1:]...)  
+	cmd.Stdin = os.Stdin  
+	cmd.Stdout = os.Stdout  
+	cmd.Stderr = os.Stderr  
+	cmd.Env = append(os.Environ(), envVars...)  
+	return cmd  
 }
