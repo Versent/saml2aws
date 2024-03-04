c1985e3f17362922017b0a4830f2d6925415cd62
Rich Lafferty <rlafferty@pagerduty.com>
2024-03-04 19:40:20 -0400

    Only append Passcode MFA type once

--- pkg/provider/okta/okta.go
+++ pkg/provider/okta/okta.go
@@ -973,10 +973,10 @@ func verifyMfa(oc *Client, oktaOrgHost string, loginDetails *creds.LoginDetails,
 
 		if doc.Find("option[value=\"phone1\"]").Length() > 0 {
 			duoMfaOptions = append(duoMfaOptions, "Duo Push")
-			duoMfaOptions = append(duoMfaOptions, "Passcode")
 		}
 
-		if doc.Find("option[value=\"token\"]").Length() > 0 {
+		if doc.Find("option[value=\"token\"]").Length() > 0 ||
+			doc.Find("option[value=\"phone1\"]").Length() > 0 {
 			duoMfaOptions = append(duoMfaOptions, "Passcode")
 		}
 
